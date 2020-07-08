5c6e3bf9816e0b895cdc56498bb1d67a808c3783
Phuc Trung <mygalaxy712@gmail.com>
2020-07-08 21:27:47 +0700

    Implemented set state in context

--- src/components/BlogEditor/BlogEditor.js
+++ src/components/BlogEditor/BlogEditor.js
@@ -1,32 +1,34 @@
 import React, { Component } from 'react';
-import {AppProvider,AppConsumer} from '../../contexts/AppContext.js';
-import {MenuConsumer} from '../../contexts/MenuContext.js';
-import { Container,Row,Col } from 'react-bootstrap';
+import { AppProvider, AppConsumer } from '../../contexts/AppContext.js';
+import { MenuConsumer } from '../../contexts/MenuContext.js';
+import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 class BlogEditor extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
+    render() {
         return <AppProvider>
-
             <Container fluid>
+                <div style={{ height: '4rem', borderBottom: '0.25rem solid black' }}></div>
                 <Row>
                     <Col sm={10}>
                         <AppConsumer>
-                            {({editorComponent})=>editorComponent}
+                            {({ editorComponent }) => editorComponent}
                         </AppConsumer>
                     </Col>
-                    <Col style={{backgroundColor:'#fff3e0',height:'100vh'}}>
-                        <MenuConsumer>
-                            {({menuComponent})=>menuComponent}
-                        </MenuConsumer>
+                    <Col>
+                        <div style={{ backgroundColor: '#fff3e0', height: '100vh' }}>
+                            <MenuConsumer>
+                                {({ menuComponent }) => menuComponent}
+                            </MenuConsumer>
+                        </div>
                     </Col>
                 </Row>
             </Container>
         </AppProvider>;
     }
 }
- 
+
 export default BlogEditor;
--- src/contexts/MenuContext.js
+++ src/contexts/MenuContext.js
@@ -2,11 +2,16 @@ import React from 'react';
 const MenuContext = React.createContext();
 const MenuConsumer = MenuContext.Consumer;
 class MenuProvider extends React.Component {
+    constructor(props){
+        super(props);
+        this.setState = this.setState.bind(this);
+    }
     state = { 
-        menuComponent : <h2>Menu</h2>
-     }
+        menuComponent : <h2>Menu</h2>,
+    }
+
     render() { 
-        return (<MenuContext.Provider value={{...this.state}}>
+        return (<MenuContext.Provider value={{...this.state,setState: this.setState}}>
             {this.props.children}
         </MenuContext.Provider> );
     }
